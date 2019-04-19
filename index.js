eb0d5d0dd1bc84da481df2ba27b1b90aa78ae957
Lawson Hung <lhung1@babson.edu>
2019-04-19 23:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,5 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newSlicedArray = new Array;
-  return kittens.slice(kittens.length, 1);
+  return kittens.slice(0, kittens.length - 1);
 }
